e664dce14ff2ff451bc97811f472167e1fb42c72
Allen Wu <allen.wu72@gmail.com>
2024-06-21 00:53:40 -0400

    add gitignore

--- cb.py
+++ cb.py
@@ -3,7 +3,7 @@ from vowpalwabbit import LabelType, Workspace
 import pandas as pd
 import numpy as np
 
-def get_cost(y, action):
+def get_cost(y: int, action: int):
     if y == action:
         return -1
     return 0
@@ -18,25 +18,27 @@ def to_vw_format(context: pd.Series, label=None):
         string += f"{feature} "
     return string
 
-def get_action(vw, context, actions):
-    context = to_vw_format(context)
+def get_action(vw: Workspace, x: np.ndarray, A: list):
+    context = to_vw_format(x)
     probs = vw.predict(context)
     a = choices(range(len(probs)), weights=probs, k=1)[0]
-    return actions[a], probs[a]
+    return A[a], probs[a]
 
-def simulate_once(model: Workspace, x: pd.DataFrame, y, actions, learn):
-    a, p = get_action(model, x, actions)
+def simulate_once(model: Workspace, x: np.ndarray, y: int, A: list, learn: bool):
+    a, p = get_action(model, x, A)
     cost = get_cost(y, a)
 
     if learn:
         label = (a, cost, p)
         vw_format = model.parse(to_vw_format(x, label), LabelType.CONTEXTUAL_BANDIT)
         model.learn(vw_format)
-    return cost
+    return a, cost
 
-def simulate(model: Workspace, Xs: np.ndarray, ys: np.ndarray, actions: list, learn=True):
+def simulate(model: Workspace, Xs: np.ndarray, ys: np.ndarray, A: list, learn=True):
+    actions = []
     costs = []
     for x, y in zip(Xs, ys):
-        cost = simulate_once(model, x, y, actions, learn)
+        a, cost = simulate_once(model, x, y, A, learn)
+        actions.append(a)
         costs.append(cost)
-    return costs
+    return actions, costs
