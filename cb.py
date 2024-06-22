77ec44256b1c366c7f8d39137b72ee6be8368bcb
Allen Wu <allen.wu72@gmail.com>
2024-06-21 22:49:13 -0400

    Autoformat code

--- cb.py
+++ cb.py
@@ -4,10 +4,12 @@ import pandas as pd
 import numpy as np
 from tqdm import tqdm
 
+
 def get_cost(y: int, action: int):
     if y == action:
         return -1
     return 0
+
 
 def to_vw_format(context: pd.Series, label=None):
     string = ""
@@ -19,11 +21,13 @@ def to_vw_format(context: pd.Series, label=None):
         string += f"{feature} "
     return string
 
+
 def get_action(vw: Workspace, x: np.ndarray, A: list):
     context = to_vw_format(x)
     probs = vw.predict(context)
     a = choices(range(len(probs)), weights=probs, k=1)[0]
     return A[a], probs[a]
+
 
 def simulate_once(model: Workspace, x: np.ndarray, y: int, A: list, learn: bool):
     a, p = get_action(model, x, A)
@@ -35,21 +39,24 @@ def simulate_once(model: Workspace, x: np.ndarray, y: int, A: list, learn: bool)
         model.learn(vw_format)
     return a, cost
 
+
 def shuffle(Xs: np.ndarray, ys: np.ndarray):
     indices = np.random.permutation(len(Xs))
     return Xs[indices], ys[indices]
 
+
 def simulate(model: Workspace, Xs: np.ndarray, ys: np.ndarray, A: list, learn=True):
     actions = []
     costs = []
-    
-    if not learn:
+
+    if learn:
         Xs, ys = shuffle(Xs, ys)
     for x, y in tqdm(zip(Xs, ys), total=len(Xs)):
         a, cost = simulate_once(model, x, y, A, learn)
         actions.append(a)
         costs.append(cost)
     return actions, costs
+
 
 def evaluate(model: Workspace, Xs: np.ndarray, A: list):
     actions = []
--- utils.py
+++ utils.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder, OrdinalEncoder
+from sklearn.preprocessing import LabelEncoder, OrdinalEncoder, RobustScaler
 from IPython.display import display
 import math
 from pandas.api.types import is_object_dtype
 import seaborn as sns
 import numpy as np
 
-def preprocess(df: pd.DataFrame, x_encoder: OrdinalEncoder=None, y_encoder: LabelEncoder=None, test=False):
+
+def preprocess(
+    df: pd.DataFrame,
+    x_encoder: OrdinalEncoder = None,
+    y_encoder: LabelEncoder = None,
+    test=False,
+):
     if not test:
         y, y_encoder = encode_y(df["NObeyesdad"], y_encoder)
         y += 1
@@ -16,16 +22,12 @@ def preprocess(df: pd.DataFrame, x_encoder: OrdinalEncoder=None, y_encoder: Labe
     X = df.set_index("id")
     X, x_encoder = encode_x(X, x_encoder)
     X = feature_engineering(X)
-
-    # if x_scaler is None:
-    #     x_scaler = RobustScaler()
-    #     x_scaler.set_output(transform="pandas")
-    #     x_scaler.fit(X)
-    # X = x_scaler.transform(X)
+    # X = scale_x(X, x_scaler)
 
     if not test:
         return X, y, x_encoder, y_encoder
     return X, x_encoder, y_encoder
+
 
 def encode_y(y: pd.Series, y_encoder: LabelEncoder):
     if y_encoder is None:
@@ -34,6 +36,7 @@ def encode_y(y: pd.Series, y_encoder: LabelEncoder):
     y = y_encoder.transform(y)
     y = pd.Series(y, name="NObeyesdad")
     return y, y_encoder
+
 
 def encode_x(X: pd.DataFrame, x_encoder: OrdinalEncoder):
     encoded_cols = []
@@ -50,47 +53,62 @@ def encode_x(X: pd.DataFrame, x_encoder: OrdinalEncoder):
     X[encoded_names] = x_encoder.transform(pd.concat(encoded_cols, axis=1))
     return X, x_encoder
 
+
 def feature_engineering(X: pd.DataFrame):
-    X["BMI"] = X["Weight"]/X["Height"]**2
+    X["BMI"] = X["Weight"] / X["Height"] ** 2
     X["BMI_group"] = group_series(X["BMI"], [18.5, 25, 30, 35, 40])
     X["FAVC-FCVC"] = X["FAVC"] - X["FCVC"]
     X["BMI*FAF"] = X["BMI"] * X["FAF"]
     X["FAF-TUE"] = X["FAF"] - X["TUE"]
     X["FCVC*NCP"] = X["FCVC"] * X["NCP"]
-    X["BMI/NCP"] = X["BMI"]/X["NCP"]
+    X["BMI/NCP"] = X["BMI"] / X["NCP"]
     X["Age_group"] = group_series(X["Age"], [10, 20, 30, 40, 50, 60, 70])
     return X
+
+
+def scale_x(X: pd.DataFrame, x_scaler: RobustScaler):
+    if x_scaler is None:
+        x_scaler = RobustScaler()
+        x_scaler.set_output(transform="pandas")
+        x_scaler.fit(X)
+    X = x_scaler.transform(X)
+    return X
+
 
 def group_series(series: pd.Series, bounds: list[int]):
     labels = list(range(len(bounds)))
     conditions = [series < bound for bound in bounds]
     return np.select(conditions, labels, len(labels))
 
+
 def costs_to_moving_avg(costs: list):
     total_cost = 0
     avg_costs = []
     for i, cost in enumerate(costs):
         total_cost += cost
-        avg_costs.append(-total_cost/(i+1))
+        avg_costs.append(-total_cost / (i + 1))
     return avg_costs
+
 
 def get_num_rows_cols(total: int):
     num_rows = int(math.ceil(math.sqrt(total)))
     while total % num_rows != 0:
         num_rows -= 1
-    num_cols = total//num_rows
+    num_cols = total // num_rows
     return num_rows, num_cols
+
 
 def get_ax(i: int, axes, num_rows: int, num_cols: int):
     if num_cols == 1 & num_rows == 1:
         ax = axes
     elif num_cols == 1:
-        ax = axes[i%num_cols]
+        ax = axes[i % num_cols]
     elif num_rows == 1:
-        ax = axes[i%num_cols]
+        ax = axes[i % num_cols]
     else:
-        ax = axes[i//num_cols, i%num_cols]
+        ax = axes[i // num_cols, i % num_cols]
     return ax
+
 
 def plot_categorical_distr(df: pd.DataFrame, cols: list[str], figsize: tuple[int, int]):
     num_rows, num_cols = get_num_rows_cols(len(cols))
@@ -100,9 +118,13 @@ def plot_categorical_distr(df: pd.DataFrame, cols: list[str], figsize: tuple[int
         counts = df[col].value_counts()
         counts.sort_index().plot.bar(ax=ax, figsize=figsize)
 
+
 def plot_numerical_distr(df: pd.DataFrame, cols: list[str], figsize: tuple[int, int]):
     num_rows, num_cols = get_num_rows_cols(len(cols))
-    df[cols].plot.kde(layout=(num_rows, num_cols), subplots=True, sharex=False, figsize=figsize)
+    df[cols].plot.kde(
+        layout=(num_rows, num_cols), subplots=True, sharex=False, figsize=figsize
+    )
+
 
 def plot_average_costs(avg_costs: list):
     plt.plot(range(1, len(avg_costs) + 1), avg_costs)
@@ -110,6 +132,7 @@ def plot_average_costs(avg_costs: list):
     plt.ylabel("moving average cost", fontsize=14)
     plt.title("moving average cost vs iteration")
     plt.ylim([0, 1])
+
 
 def print_stats(df: pd.DataFrame):
     print("head")
@@ -119,9 +142,16 @@ def print_stats(df: pd.DataFrame):
     print("info")
     print(df.info())
 
+
 def get_top_correlations(df: pd.DataFrame, threshold: 0.9):
-    lower_corr = df.corr().reset_index().melt(id_vars="index", value_name="corr").loc[lambda df: df["index"] < df["variable"]]
+    lower_corr = (
+        df.corr()
+        .reset_index()
+        .melt(id_vars="index", value_name="corr")
+        .loc[lambda df: df["index"] < df["variable"]]
+    )
     return lower_corr.loc[lambda df: df["corr"] > threshold]
+
 
 def plot_correlation(df: pd.DataFrame):
     sns.heatmap(df.corr())
