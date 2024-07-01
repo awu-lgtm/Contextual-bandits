18dec764ba69b182641854acc5f58c3a4e30fd95
Allen Wu <allen.wu72@gmail.com>
2024-07-01 13:00:23 -0400

    obd changes
    - add a few functions to obd
    - rename obd2.ipynb to obd

--- obd.py
+++ obd.py
@@ -2,6 +2,9 @@ import pandas as pd
 from os.path import join
 from sklearn.preprocessing import OrdinalEncoder
 import numpy as np
+from tqdm import tqdm
+from cb import to_vw_adf_format
+from vowpalwabbit import LabelType, Workspace
 
 y_name = "item_id"
 cost_name = "click"
@@ -11,6 +14,7 @@ data_path = "open_bandit_dataset"
 bts_path = join(data_path, "bts")
 bts_all_path = join(bts_path, "all")
 bts_dat_path = join(bts_all_path, "all.dat")
+bts_item_context_path = join(bts_all_path, "item_context.csv")
 
 bts_csv_path = join(data_path, "bts", "all", "all.csv")
 random_csv_path = join(data_path, "random", "all", "all.csv")
@@ -25,6 +29,15 @@ def to_parquet(path="bts"):
         pd.read_csv(random_csv_path).to_parquet(random_parquet_path)
 
 
+def make_encoder_and_encode(df: pd.DataFrame, encoder: OrdinalEncoder):
+    if encoder is None:
+        encoder = OrdinalEncoder()
+        encoder.set_output(transform="pandas")
+        encoder.fit(df)
+    df = encoder.transform(df)
+    return df, encoder
+
+
 def preprocess(df: pd.DataFrame, x_encoder: OrdinalEncoder = None):
     dt = pd.to_datetime(df["timestamp"], format="mixed")
     df["hour"] = dt.dt.hour
@@ -32,15 +45,20 @@ def preprocess(df: pd.DataFrame, x_encoder: OrdinalEncoder = None):
     df = df.drop(["timestamp", "Unnamed: 0"], axis=1)
 
     y, cost, prob = df[y_name], df[cost_name], df[prob_name]
+    cost = 1 - cost
     df = df.drop([y_name, cost_name, prob_name], axis=1)
 
-    if x_encoder is None:
-        x_encoder = OrdinalEncoder()
-        x_encoder.set_output(transform="pandas")
-        x_encoder.fit(df)
-    df = x_encoder.transform(df)
+    df, x_encoder = make_encoder_and_encode(df, x_encoder)
     df = pd.concat([df, y, cost, prob], axis=1)
     return df, x_encoder
+
+
+def preprocess_item_context(df: pd.DataFrame, encoder: OrdinalEncoder = None):
+    ids = df["item_id"]
+    df = df.drop(["item_id"], axis=1)
+    df, encoder = make_encoder_and_encode(df, encoder)
+    df = pd.concat([ids, df], axis=1)
+    return df, encoder
 
 
 def downcast(df: pd.DataFrame):
@@ -48,3 +66,17 @@ def downcast(df: pd.DataFrame):
         if name != prob_name:
             df[name] = col.astype(np.int8)
     return df
+
+
+def train_model(model: Workspace, df: pd.DataFrame, action_context: pd.DataFrame):
+    for _, row in tqdm(df.iterrows(), total=len(df)):
+        action, cost, prob = row[y_name], row[cost_name], row[prob_name]
+        context = row.drop([y_name, cost_name, prob_name])
+        df[y_name], df[cost_name], df[prob_name]
+        label = (action, cost, prob)
+        example = model.parse(
+            to_vw_adf_format(context, action_context, label),
+            LabelType.CONTEXTUAL_BANDIT,
+        )
+        model.learn(example)
+        model.finish_example(example)
